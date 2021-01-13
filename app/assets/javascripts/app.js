328a365b0694740e8cae1726bfccf7020a67fcdf
EC2 Default User <ec2-user@ip-172-31-38-133.ap-northeast-1.compute.internal>
2021-01-13 12:11:01 +0000

    クイズ1問目を実装とリファクタリング

--- app/assets/javascripts/app.js
+++ app/assets/javascripts/app.js
@@ -8,7 +8,37 @@ const answers = [
 const correct = 'ソフトウェア';
 
 function question_select() {
-  document.getElementById('js-question').textContent = question;gi
+  
+  const $button = document.getElementsByTagName('button');
+  const buttonLength = $button.length;
+  //クイズの問題文、選択肢を定義
+  const setupQuiz =() => {
+    document.getElementById('js-question').textContent = question;
+    let buttonIndex = 0;
+    let buttonLength = $button.length;
+    while(buttonIndex < buttonLength){
+      $button[buttonIndex].textContent = answers[buttonIndex];
+      buttonIndex++;
+    }
+  }
+  setupQuiz();
+ 
+  const clickHandler = (e) => {
+    if(correct === e.target.textContent){
+      window.alert('正解！');
+    } else {
+      window.alert('不正解！');
+    }
+  };
+  //ボタンをクリックしたら正誤判定
+  let handlerIndex = 0;
+  while (handlerIndex < buttonLength) {
+    $button[handlerIndex].addEventListener('click',　(e) =>{
+    clickHandler(e);
+    });
+    
+    handlerIndex++;
+  }
 }
 
 window.addEventListener("load", question_select);
